3d238b3b5298a7157a6357e22f84a4384ae431d5
JINNAD UL RAIHAN CHOWDHURY <jinnadulraihan47848@gmail.com>
2026-06-26 23:24:24 +0530

    Time: 68 ms (56.95%), Space: 114.8 MB (48.34%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mp;// cumSum | count
+        mp[0] = 1;
+
+        int cumSum = 0;
+        long long count_subarray = 0;
+        long long validLeftPoints = 0;
+
+        for(int j = 0; j<n; ++j){
+            if(nums[j] == target){
+                validLeftPoints += mp[cumSum];
+                cumSum++;
+            }
+            else{
+                cumSum--;
+                validLeftPoints -= mp[cumSum];
+            }
+
+            mp[cumSum]++;
+            count_subarray += validLeftPoints;
+        }
+
+        
+        return count_subarray;
+    }
+};
