3c32ca6f7bb460648314ae35764e4bd77f40ca8b
JINNAD UL RAIHAN CHOWDHURY <jinnadulraihan47848@gmail.com>
2026-06-29 21:54:17 +0530

    Time: 191 ms (100%), Space: 34.3 MB (75%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.py
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.py
@@ -3,9 +3,10 @@ class Solution:
         nums.sort(reverse=True)
         sum = 0
         for i in range(0,k,1):
-            op1 = nums[i] * mul
-            op2 = nums[i]
-            sum += max(op1, op2)
+            if mul > 0:
+                sum += (nums[i] * mul)
+            else:
+                sum += nums[i]
             mul -= 1
         
         return sum
