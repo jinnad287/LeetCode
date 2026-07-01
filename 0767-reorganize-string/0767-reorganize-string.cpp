69b5140c9f6aaf9fa62812bee698fb2d60137125
JINNAD UL RAIHAN CHOWDHURY <jinnadulraihan47848@gmail.com>
2026-07-02 01:25:32 +0530

    Time: 1 ms (32.97%), Space: 8.5 MB (55.58%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char, int> mp;
+        for(char& ch: s){
+            mp[ch]++;
+        }
+        
+        // maxheap to dynamically sort 'char' by 'freq'
+        priority_queue<pair<int, char>> pq;
+        for(auto& [ch, freq] : mp){
+            pq.push({freq, ch});
+        }
+
+        string ans = "";
+
+        // taking to 2 characters from heap to prevent adjancency problem properly
+        while(pq.size() >= 2){
+            auto [freq1, ch1] = pq.top(); pq.pop();
+            auto [freq2, ch2] = pq.top(); pq.pop();
+
+            ans += ch1;
+            ans += ch2;
+
+            // after using the char one time decrease their freq by 1
+            // and push them again if their freq is not zero
+            if(--freq1 > 0) pq.push({freq1, ch1});
+            if(--freq2 > 0) pq.push({freq2, ch2});
+        }
+
+        // if pq not empty then it must be the one char left
+        // and its freq must be 1, otherwise it not possible
+        if(!pq.empty()){
+            auto [freq, ch] = pq.top();
+            if(freq > 1) return "";
+            ans += ch;
+        }
+
+        return ans;
+        
+    }
+};
