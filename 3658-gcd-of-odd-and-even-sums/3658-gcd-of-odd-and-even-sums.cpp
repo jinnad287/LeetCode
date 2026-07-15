76c5b72dd83d4958cc89d9d4c078224eaa12408f
JINNAD UL RAIHAN CHOWDHURY <jinnadulraihan47848@gmail.com>
2026-07-15 20:21:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // sumOdd = n * n;
+        // sumEven = n * (n+1);
+        // here we can see that the common between is n
+        // which is the GCD
+        // so gcd(sumOdd, sumEven) = n
+
+        return n; 
+    }
+};
