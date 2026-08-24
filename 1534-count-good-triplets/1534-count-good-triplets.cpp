7a297e2682dcd9861daf810e8abf52269c3e0be9
JINNAD UL RAIHAN CHOWDHURY <jinnadulraihan47848@gmail.com>
2026-08-25 04:00:14 +0530

    Time: 9 ms (40.36%), Space: 10.8 MB (94.83%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -6,13 +6,11 @@ public:
 
         for(int i=0; i<n-2; ++i){
             for(int j = i+1; j<n-1; ++j){
+                if(abs(arr[i] - arr[j]) > a) continue;
                 for(int k =j+1; k<n; ++k){
-                    if(abs(arr[i] - arr[j]) <= a && 
-                       abs(arr[j] - arr[k]) <= b &&
-                       abs(arr[k] - arr[i]) <= c)
-                       {
+                    if(abs(arr[j] - arr[k]) <= b && abs(arr[k] - arr[i]) <= c){
                         ans++;
-                       }
+                    }
                 }
             }
         }
